bf3ba2097bdad6f722c8ba62e698a61cfa47a7c5
Josiane Rocha <josi1992@gmail.com>
2022-08-27 01:36:01 -0300

    chore:initial project configuration with react vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Rotas  from './Routes/Routes'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Rotas/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Rotas = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route />
+                <Route />
+                <Route />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Rotas

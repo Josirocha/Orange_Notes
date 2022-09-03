347d29dfd2f0920e764d85909ecf893d7ebda3ef
Josiane Rocha <josi1992@gmail.com>
2022-09-03 13:17:58 -0300

    feat: add createCategory component

---
+++ src/Components/CreateCategory/CreateCategory.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import CreateNewFolderOutlinedIcon from '@mui/icons-material/CreateNewFolderOutlined';
+import { Paper, Typography } from '@mui/material'
+
+const CreateCategory = () => {
+    return (
+        <Paper elevation={3}
+            color='secondary'
+            sx={{
+                width: '180px',
+                borderRadius: '16px',
+                marginTop: '16px',
+                padding: '16px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                background: '#ff8e3c',
+                cursor: 'pointer'
+            }}>
+            <CreateNewFolderOutlinedIcon color='primary' size={50} />
+            <Typography variant='h5' sx={{ ml: 2 }} color='primary' >
+                Novo
+            </Typography>
+        </Paper>
+    )
+}
+
+export default CreateCategory

18a85373f3b73c268de04e0f845a9e685877d1bc
Josiane Rocha <josi1992@gmail.com>
2022-09-03 20:22:47 -0300

    feat: adding onClick event and lint

--- src/Components/CreateCategory/CreateCategory.jsx
+++ src/Components/CreateCategory/CreateCategory.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import CreateNewFolderOutlinedIcon from '@mui/icons-material/CreateNewFolderOutlined';
-import { Paper, Typography } from '@mui/material'
+import React from "react";
+import CreateNewFolderOutlinedIcon from "@mui/icons-material/CreateNewFolderOutlined";
+import { Paper, Typography } from "@mui/material";
 
-const CreateCategory = () => {
+const CreateCategory = ({ onClick }) => {
     return (
-        <Paper elevation={3}
-            color='secondary'
+        <Paper
+            onClick={onClick}
+            elevation={3}
+            color="secondary"
             sx={{
-                width: '180px',
-                borderRadius: '16px',
-                marginTop: '16px',
-                padding: '16px',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                background: '#ff8e3c',
-                cursor: 'pointer'
-            }}>
-            <CreateNewFolderOutlinedIcon color='primary' size={50} />
-            <Typography variant='h5' sx={{ ml: 2 }} color='primary' >
+                width: "180px",
+                borderRadius: "16px",
+                marginTop: "16px",
+                padding: "16px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                background: "#ff8e3c",
+                cursor: "pointer",
+            }}
+        >
+            <CreateNewFolderOutlinedIcon color="primary" size={50} />
+            <Typography variant="h5" sx={{ ml: 2 }} color="primary">
                 Novo
             </Typography>
         </Paper>
-    )
-}
+    );
+};
 
-export default CreateCategory
+export default CreateCategory;

f053431308cec3ff3b731aa867519643f2bcfe16
Josiane Rocha <josi1992@gmail.com>
2022-09-04 12:23:33 -0300

    feat adding page Notes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
 import Categories from '../Pages/Categories/Categories';
+import Notes from '../Pages/Notes/Notes';
 
 const Routes= () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route  path="/" exact element={<Categories/>}  />
-                <Route />
+                <Route path="notes/:id" exact element={<Notes/>}/>
                 <Route />
             </Switch>
         </BrowserRouter>
